946744ea76f7a2ed095eb4fd5bb0c08305269c0c
Keyel <keyel@localhost.localdomain>
2023-11-26 03:24:08 -0600

    [ADD] Objeto Cliente

--- src/main/java/com/example/lavanderiawinchi/models/Cliente.java
+++ src/main/java/com/example/lavanderiawinchi/models/Cliente.java
@@ -2,4 +2,11 @@ package com.example.lavanderiawinchi.models;
 
 public class Cliente {
 
+    public int maquinaAsignada;
+    public int tiempoDeLavado;
+
+    public Cliente(int tL){
+        this.maquinaAsignada = -1;
+        this.tiempoDeLavado = tL;
+    }
 }
