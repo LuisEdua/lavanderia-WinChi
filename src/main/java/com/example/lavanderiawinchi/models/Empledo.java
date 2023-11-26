04050eb70060671e628cf1b8a70b7be500bd7d85
Keyel <keyel@localhost.localdomain>
2023-11-26 03:27:56 -0600

    [ADD] Objeto Empleado

--- src/main/java/com/example/lavanderiawinchi/models/Empledo.java
+++ src/main/java/com/example/lavanderiawinchi/models/Empledo.java
@@ -1,4 +1,13 @@
 package com.example.lavanderiawinchi.models;
 
 public class Empledo {
+
+    public boolean disponible;
+    public int maquinaEnReparacion;
+
+    public Empledo(){
+        this.disponible = true;
+        this.maquinaEnReparacion = -1;
+    }
+
 }
