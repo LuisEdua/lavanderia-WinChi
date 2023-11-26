4fad71e87b6b9d97f7d603315947d877626b0a59
Keyel <keyel@localhost.localdomain>
2023-11-26 04:27:33 -0600

    [UPDATE] Objeto Monitor

--- src/main/java/com/example/lavanderiawinchi/models/Monitor.java
+++ src/main/java/com/example/lavanderiawinchi/models/Monitor.java
@@ -6,4 +6,15 @@ public class Monitor {
 
     private MaquinaDeLavado[] maquinas = new MaquinaDeLavado[20];
 
+    private Cliente c;
+    private Empledo e;
+    private MaquinaDeLavado m;
+
+    public Monitor(Cliente c, Empledo e, MaquinaDeLavado m){
+        this.c=c;
+        this.e=e;
+        this.m=m;
+        this.capacidad=20;
+    }
+
 }
